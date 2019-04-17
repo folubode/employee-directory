c911c450412a98751509e9f82469f574f39a7d39
Femi Olubode-Awosola <folubode@gmail.com>
2019-04-17 12:49:03 +1200

    Init..

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-	match 'employees/directory', :to => 'employees#directory'
-	match 'employees/menu', :to => 'employees#menu'
-	match 'employees/extension', :to => 'employees#extension'
-	match 'employees/call', :to => 'employees#call'
+	get 'employees/directory', :to => 'employees#directory'
+	get 'employees/menu', :to => 'employees#menu'
+	get 'employees/extension', :to => 'employees#extension'
+	get 'employees/call', :to => 'employees#call'
 	
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
